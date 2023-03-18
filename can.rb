584d7b84d35630b5d800321bb248eb2a0938b410
Josh Voigts <joshvoigts@gmail.com>
2023-03-18 14:42:42 -0500

    Fix incorrect sha

--- can.rb
+++ can.rb
@@ -2,7 +2,7 @@ class Can < Formula
   desc "Cross-platform command line tool for moving files to the trash"
   homepage "https://github.com/joshvoigts/can"
   url "https://github.com/joshvoigts/can/releases/download/v0.1.0/can-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "86c8951f5d055f2f6df29d4093dbf7ed1a658730"
+  sha256 "4bbf5c8159a0bf2a5428c44872fb3d5eea4580f06c573c77c5697ebe99d6883a"
   version "0.1.0"
 
   def install
