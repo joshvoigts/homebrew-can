eb472e517075672e51699966774b16b5a80b36bd
Josh Voigts <joshvoigts@gmail.com>
2023-03-18 21:05:53 -0500

    Update release hash

--- can.rb
+++ can.rb
@@ -2,7 +2,7 @@ class Can < Formula
   desc "Cross-platform command line tool for moving files to the trash"
   homepage "https://github.com/joshvoigts/can"
   url "https://github.com/joshvoigts/can/releases/download/v0.1.0/can-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "4bbf5c8159a0bf2a5428c44872fb3d5eea4580f06c573c77c5697ebe99d6883a"
+  sha256 "2a07349f6c16d04dd2b4f0cae87c2277d54e9333056b87602e05711af1269b61"
   version "0.1.0"
 
   def install
